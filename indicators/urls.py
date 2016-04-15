f76dc58ee2dd52c1167a2c7c5ae97cc462c37511
Greg Lind <gwlind@gmail.com>
2016-04-15 09:41:08 -0700

    fix indicator report export

--- indicators/urls.py
+++ indicators/urls.py
@@ -40,7 +40,6 @@ urlpatterns = [
     url(r'^data/(?P<id>\w+)/(?P<program>\w+)/table/$', 'indicators.views.indicator_data_report', name='indicator_data_report'),
     url(r'^data/(?P<id>\w+)/(?P<program>\w+)/$', 'indicators.views.indicator_data_report', name='indicator_data_report'),
     url(r'^export/(?P<id>\w+)/(?P<program>\w+)/$', IndicatorExport.as_view(), name='indicator_export'),
-    url(r'^export/$', IndicatorExport.as_view(), name='indicator_export'),
 
     #ajax calls
     url(r'^service/(?P<service>[-\w]+)/service_json/', 'indicators.views.service_json', name='service_json'),
