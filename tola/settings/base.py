2592153a3aa5298b39bb1bbe058a6401bb5f2c43
Greg Lind <gwlind@gmail.com>
2016-05-11 14:31:43 -0700

    update customdashboard with new survey, import countries

--- tola/settings/base.py
+++ tola/settings/base.py
@@ -208,7 +208,6 @@ DJANGO_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     # Uncomment the next line to enable the admin:
-    'flat',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
