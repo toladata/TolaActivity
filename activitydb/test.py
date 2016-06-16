9f933aae3ce2cf3d99c48a632c6caf3c3bfae348
minedamnesia <minedamnesia@gmailcom>
2016-06-16 10:36:15 -0700

    flush out distribution test

--- activitydb/test.py
+++ activitydb/test.py
@@ -163,35 +163,50 @@ class TrainingAttendanceTestCase(TestCase):
         self.assertEqual(TrainingAttendance.objects.filter(id=get_training.id).count(), 1)
 
 
-# class DistributionTestCase(TestCase):
+class DistributionTestCase(TestCase):
 
-#     def setUp(self):
-#         new_program = Program.objects.create(name="testprogram")
-#         new_program.save()
-#         get_program = Program.objects.get(name="testprogram")
-    #     new_distribution = Distribution.objects.create(training_name="testtraining", program=get_program,
-    #                                                        implementer = "34",
-    #                                                        reporting_period = "34",
-    #                                                        start_date = "34",
-    #                                                        end_date = "34",
-    #                                                        trainer_name = "34",
-    #                                                        trainer_contact_num = "34",
-    #                                                        form_filled_by = "34",
-    #                                                        form_filled_by_contact_num = "34",
-    #                                                        total_male = "34",
-    #                                                        total_female = "34",
-    #                                                        total_age_0_14_male = "34",
-    #                                                        total_age_0_14_female = "34",
-    #                                                        total_age_15_24_male = "34",
-    #                                                        total_age_15_24_female = "34",
-    #                                                        total_age_25_59_male = "34"
-    #                                                      )
-    #     new_distribution.save()
+    def setUp(self):
+        new_program = Program.objects.create(name="testprogram")
+        new_program.save()
+        get_program = Program.objects.get(name="testprogram")
+        new_distribution = Distribution.objects.create(distribution_name="testdistribution", program=get_program,
+                                                            activity_code = "111222",
+                                                            office_code=get_office,
+                                                            distribution_indicator = "34",
+                                                            distribution_implementer = "34",
+                                                            reporting_period = "34",
+                                                            province=get_province,
+                                                            total_beneficiaries_received_input = "34",
+                                                            distribution_location = "testlocation",
+                                                            input_type_distributed = "testinputtype",
+                                                            distributor_name_and_affiliation = "testdistributorperson",
+                                                            distributor_contact_number = "1-dis-tri-bute",
+                                                            start_date = "34",
+                                                            end_date = "34",
+                                                            form_filled_by = "34",
+                                                            form_filled_by_position = "testdistributionmanager",
+                                                            form_filled_by_contact_num = "1-888-dst-rbut"
+                                                            form_filled_date = "34",
+                                                            form_verified_by = "34",
+                                                            form_verified_by_position = "testdistributionmanager",
+                                                            form_verified_by_contact_num = "1-888-dst-rbut"
+                                                            form_verified_date = "34",
+                                                            total_received_input = "34"
+                                                            total_male = "34",
+                                                            total_female = "34",
+                                                            total_age_0_14_male = "34",
+                                                            total_age_0_14_female = "34",
+                                                            total_age_15_24_male = "34",
+                                                            total_age_15_24_female = "34",
+                                                            total_age_25_59_male = "34"
+                                                            total_age_25_59_female = "34"
+                                                         )
+        new_distribution.save()
 
-    # def test_distribution_exists(self):
-    #     """Check for Training object"""
-    #     get_training = Distribution.objects.get(distribution_name="testdistribution")
-    #     self.assertEqual(Distribution.objects.filter(id=get_distribution.id).count(), 1)
+    def test_distribution_exists(self):
+        """Check for Training object"""
+        get_training = Distribution.objects.get(distribution_name="testdistribution")
+        self.assertEqual(Distribution.objects.filter(id=get_distribution.id).count(), 1)
 
 
 class BeneficiaryTestCase(TestCase):
