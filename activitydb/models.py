e7e7411f2789a4063c0edd4c9aadd4c8c4606c99
minedamnesia <minedamnesia@gmailcom>
2016-06-16 09:34:20 -0700

    add distribution form model

--- activitydb/models.py
+++ activitydb/models.py
@@ -1357,3 +1357,57 @@ class FormGuidanceAdmin(admin.ModelAdmin):
     display = 'Form Guidance'
 
 
+
+class Distribution(models.Model):
+    distribution_name = models.CharField(max_length=255)
+    program = models.ForeignKey(Program, null=True, blank=True)
+    activity_code = models.CharField(max_length=255)
+    office_code = models.ForeignKey(Office, null=True, blank=True)
+    distribution_indicator = models.CharField(max_length=255)
+    distribution_implementer = models.CharField(max_length=255, null=True, blank=True)
+    reporting_period = models.CharField(max_length=255, null=True, blank=True)
+    province = models.ForeignKey(Province, null=True, blank=True)
+    total_beneficiaries_received_input = models.IntegerField(null=True, blank=True)
+    distribution_location = models.CharField(max_length=255, null=True, blank=True)
+    input_type_distributed = models.CharField(max_length=255, null=True, blank=True)   
+    distributor_name_and_affiliation = models.CharField(max_length=255, null=True, blank=True)   
+    distributor_contact_number = models.CharField(max_length=255, null=True, blank=True)   
+    start_date = models.CharField(max_length=255, null=True, blank=True)
+    end_date = models.CharField(max_length=255, null=True, blank=True)
+    form_filled_by = models.CharField(max_length=255, null=True, blank=True)
+    form_filled_by_position = models.CharField(max_length=255, null=True, blank=True)   
+    form_filled_by_contact_num = models.CharField(max_length=255, null=True, blank=True)   
+    form_filled_date = models.CharField(max_length=255, null=True, blank=True)   
+    form_verified_by = models.CharField(max_length=255, null=True, blank=True)
+    form_verified_by_position = models.CharField(max_length=255, null=True, blank=True)    
+    form_verified_by_contact_num = models.CharField(max_length=255, null=True, blank=True)   
+    form_verified_date = models.CharField(max_length=255, null=True, blank=True)   
+    total_received_input = models.CharField(max_length=255, null=True, blank=True)
+    total_male = models.IntegerField(null=True, blank=True)
+    total_female = models.IntegerField(null=True, blank=True)
+    total_age_0_14_male = models.IntegerField(null=True, blank=True)
+    total_age_0_14_female = models.IntegerField(null=True, blank=True)
+    total_age_15_24_male = models.IntegerField(null=True, blank=True)
+    total_age_15_24_female = models.IntegerField(null=True, blank=True)
+    total_age_25_59_male = models.IntegerField(null=True, blank=True)
+    total_age_25_59_female = models.IntegerField(null=True, blank=True)
+    create_date = models.DateTimeField(null=True, blank=True)
+    edit_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('distribution_name',)
+
+    def save(self, *args, **kwargs):
+        if self.create_date == None:
+            self.create_date = datetime.now()
+        self.edit_date = datetime.now()
+        super(Distribution, self).save()
+
+    # displayed in admin templates
+    def __unicode__(self):
+        return unicode(self.training_name)
+
+
+class DistributionAdmin(admin.ModelAdmin):
+    list_display = ('distribution_name', 'program', 'project_agreement', 'create_date', 'edit_date')
+    display = 'Program Dashboard'
