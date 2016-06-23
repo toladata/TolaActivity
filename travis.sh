c8b66b030bf8c115c762352747231b209df845da
Diego Borchers <diego@Gregs-iMac.local>
2016-06-23 16:12:43 -0700

    2 new scripts

---
+++ travis.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+
+echo about to Push
+
+git push origin docker
+echo Pushed
+sleep 75
+
+echo $(travis status)
+
+while [ $(travis status)  == started ] || [ $(travis status)  == created ]
+do
+   echo $(travis status)
+   sleep 10
+done
+
+if [ $(travis status) == failed ];
+  then
+     echo $(travis status)
+     echo Failed
+     exit
+elif [ $(travis status) == passed ];
+  then
+     echo $(travis status)
+     echo run Docker script
+     ssh activity . /home/TolaActivity/docker.sh
+else
+     echo error
+fi
