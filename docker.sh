1eeacf1a62800ecf663374eb4019dedf74d67626
Diego <dborchers001@gmail.com>
2016-06-24 14:00:02 -0700

    Docker script

--- docker.sh
+++ docker.sh
@@ -2,8 +2,9 @@
 
 ### It dockerizes automatically ###
 cd /home/TolaActivity
+git stash
 git pull origin docker
-docker-compose up mysqldb
-sleep 5
+#docker-compose up mysqldb
+#sleep 5
 docker-compose up
 
