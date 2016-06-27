1c943ba9b283721e913c24a93a04344bb096aa4e
minedamnesia <minedamnesia@gmailcom>
2016-06-27 09:32:49 -0700

    add models relating to dashbaord and sub-pieces

--- activitydb/models.py
+++ activitydb/models.py
@@ -187,11 +187,97 @@ class ContactAdmin(admin.ModelAdmin):
     search_fields = ('name','country','title','city')
 
 
+class DashboardTheme(models.Model):
+    theme_name = models.CharField("Dashboard Theme Name", max_length=255, blank=True)
+    theme_description = models.TextField("Brief Description", null=True, blank=True, help_text="What is the focus of this theme?")
+    theme_template = models.CharField("Dashboard Theme Name", max_length=255, blank=True)
+    is_public = models.BooleanField(default=False)
+    number_of_components = models.IntegerField(blank=False)
+    create_date = models.DateTimeField(null=True, blank=True)
+    edit_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('theme_name',)
+
+    # on save add create date or update edit date
+    def save(self, *args, **kwargs):
+        if self.create_date == None:
+            self.create_date = datetime.now()
+        self.edit_date = datetime.now()
+        super(DashboardTheme, self).save()
+
+    # displayed in admin templates
+    def __unicode__(self):
+        return self.theme_name
+
+class DashboardThemeAdmin(admin.ModelAdmin):
+    list_display = ('theme_name', 'theme_description', 'is_public', 'number_of_components','create_date', 'edit_date')
+    display = 'Dashboard Theme'
+
+class ComponentDataSource(models.Model):
+    data_name = models.CharField("Name of Source Data", max_length=255, blank=True)
+    data_type = models.CharField("Data Type", max_length=200, null=True, blank=True, help_text="Is this data photos? Text? Numerical data?")
+    data_source = models.URLField(max_length=200, null=True, blank=True)
+    data_filter_key = models.CharField("Color Scheme", max_length=255, blank=False)
+    create_date = models.DateTimeField(null=True, blank=True)
+    edit_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('data_name',)
+
+    # on save add create date or update edit date
+    def save(self, *args, **kwargs):
+        if self.create_date == None:
+            self.create_date = datetime.now()
+        self.edit_date = datetime.now()
+        super(DashboardDataSource, self).save()
+
+    # displayed in admin templates
+    def __unicode__(self):
+        return self.data_name
+
+class ComponentDataSourceAdmin(admin.ModelAdmin):
+    list_display = ('data_name', 'data_type','data_source','data_filter_key', 'create_date', 'edit_date')
+    display = 'Data Source'
+
+class DashboardComponent(models.Model):
+    component_name = models.CharField("Component Name", max_length=255, blank=True)
+    component_description = models.TextField("Brief Description", null=True, blank=True, help_text="What does this component do?")
+    is_public = models.BooleanField("External Public Dashboard", default=False)
+    component_type = models.CharField("Component Type", max_length=255, blank=True)
+    data_required = models.CharField("Required Data Type", max_length=255, blank=False)
+    data_sources = models.ManyToManyField(ComponentDataSource, blank=False)
+    create_date = models.DateTimeField(null=True, blank=True)
+    edit_date = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('component_name',)
+
+    # on save add create date or update edit date
+    def save(self, *args, **kwargs):
+        if self.create_date == None:
+            self.create_date = datetime.now()
+        self.edit_date = datetime.now()
+        super(DashboardComponent, self).save()
+
+    # displayed in admin templates
+    def __unicode__(self):
+        return self.component_name
+
+class DashboardComponentAdmin(admin.ModelAdmin):
+    list_display = ('component_name', 'component_description', 'component_type', 'data_required', 'create_date', 'edit_date')
+    display = 'Dashboard Components'
 # For programs that have custom dashboards. The default dashboard for all other programs is 'Program Dashboard'
+
+
 class CustomDashboard(models.Model):
     dashboard_name = models.CharField("Custom Dashboard Name", max_length=255, blank=True)
     dashboard_description = models.TextField("Brief Description", null=True, blank=True, help_text="What does this custom dashboard display to the user?")
     is_public = models.BooleanField("External Public Dashboard", default=False)
+    dashboard_theme = models.ForeignKey(DashboardTheme, blank=False, null=False)
+    dashboard_color_palette = models.CharField("Color Scheme", max_length=255, blank=False)
+    layout_dictionary = models.TextField("...", null=True, blank=True)
+    dashboard_components = models.ManyToManyField(DashboardComponent, blank=False)
     create_date = models.DateTimeField(null=True, blank=True)
     edit_date = models.DateTimeField(null=True, blank=True)
 
@@ -211,9 +297,8 @@ class CustomDashboard(models.Model):
 
 
 class CustomDashboardAdmin(admin.ModelAdmin):
-    list_display = ('dashboard_name', 'dashboard_description', 'create_date', 'edit_date')
+    list_display = ('dashboard_name', 'dashboard_description', 'is_public', 'dashboard_theme', 'dashboard_color_palette','create_date', 'edit_date')
     display = 'Custom Dashboard'
-
 
 # For programs that have custom dashboards. The default dashboard for all other programs is 'Program Dashboard'
 class FundCode(models.Model):
@@ -234,7 +319,6 @@ class FundCode(models.Model):
     # displayed in admin templates
     def __unicode__(self):
         return self.name
-
 
 class FundCodeAdmin(admin.ModelAdmin):
     list_display = ('name','program__name', 'create_date', 'edit_date')
@@ -1355,5 +1439,4 @@ class FormGuidance(models.Model):
 class FormGuidanceAdmin(admin.ModelAdmin):
     list_display = ( 'form', 'guidance', 'guidance_link', 'create_date',)
     display = 'Form Guidance'
-
 
