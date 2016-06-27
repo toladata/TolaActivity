951bf38b2197f0eb40b4f361af1983a6bd1f2969
minedamnesia <minedamnesia@gmailcom>
2016-06-27 09:32:12 -0700

    add routes for dashboard CRUD and related pieces

--- activitydb/urls.py
+++ activitydb/urls.py
@@ -96,6 +96,27 @@ urlpatterns = [
                        url(r'^country/(?P<country>[-\w]+)/country_json/', 'activitydb.views.country_json', name='country_json'),
                        url(r'^district/(?P<district>[-\w]+)/district_json/', 'activitydb.views.district_json', name='district_json'),
 
+                       url(r'^custom_dashboard/(?P<pk>[0-9]+)/$', CustomDashboardList.as_view(), name='custom_dashboard_list'),
+                       # url(r'^custom_dashboard_detail/(?P<id>[0-9]+)/$',  CustomDashboardDetail.as_view(), name='custom_dashboard_detail'),
+                       url(r'^custom_dashboard_add/(?P<pk>[0-9]+)/$',  CustomDashboardCreate.as_view(), name='custom_dashboard_add'),
+                       # url(r'^custom_dashboard_update/(?P<pk>[0-9]+)/$',  CustomDashboardUpdate.as_view(), name='custom_dashboard_update'),
+                       # url(r'^custom_dashboard_delete(?P<pk>[0-9]+)/$',  CustomDashboardDelete.as_view(), name='custom_dashboard_delete'),
+
+                       url(r'^custom_dashboard/theme/(?P<id>[0-9]+)/$', DashboardThemeList.as_view(), name='dashboard_theme_list'),
+                       # url(r'^custom_dashboard/theme_add/(?P<pk>[0-9]+)/$',  CustomDashboardCreate.as_view(), name='custom_dashboard/theme_add'),
+                       # url(r'^custom_dashboard/theme_update/(?P<id>[0-9]+)/$',  CustomDashboardUpdate.as_view(), name='custom_dashboard/theme_update'),
+                       # url(r'^custom_dashboard/theme_delete(?P<id>[0-9]+)/$',  CustomDashboardDelete.as_view(), name='custom_dashboard/theme_delete'),
+                      
+                       url(r'^custom_dashboard/component/(?P<id>[0-9]+)/$', DashboardComponentList.as_view(), name='dashboard_component_list'),                       
+                       # url(r'^custom_dashboard/component_add/(?P<pk>[0-9]+)/$',  CustomDashboardCreate.as_view(), name='custom_dashboard/component_add'),
+                       # url(r'^custom_dashboard/component_update/(?P<id>[0-9]+)/$',  CustomDashboardUpdate.as_view(), name='custom_dashboard/component_update'),
+                       # url(r'^custom_dashboard/component_delete(?P<id>[0-9]+)/$',  CustomDashboardDelete.as_view(), name='custom_dashboard/component_delete'),
+                      
+                       url(r'^custom_dashboard/data/(?P<id>[0-9]+)/$', ComponentDataSourceList.as_view(), name='component_data_source_list'),
+                       # url(r'^custom_dashboard/data_add/(?P<pk>[0-9]+)/$',  CustomDashboardCreate.as_view(), name='custom_dashboard/data_add'),
+                       # url(r'^custom_dashboard/data_update/(?P<id>[0-9]+)/$',  CustomDashboardUpdate.as_view(), name='custom_dashboard/data_update'),
+                       # url(r'^custom_dashboard/data_delete(?P<id>[0-9]+)/$',  CustomDashboardDelete.as_view(), name='custom_dashboard/data_delete'),
+                       
                        #ajax calls
                        url(r'^service/(?P<service>[-\w]+)/service_json/', 'indicators.views.service_json', name='service_json'),
 
