73765751cf4a20c671c7affdceeec237c6ead8d9
Kelly Yamamoto <minedamnesia@gmail.com>
2016-10-19 09:48:13 -0700

    fix spacing

--- activitydb/urls.py
+++ activitydb/urls.py
@@ -115,8 +115,8 @@ urlpatterns = [
                        url(r'^custom_dashboard_delete/(?P<pk>[0-9]+)/$', CustomDashboardDelete.as_view(), name='custom_dashboard_delete'),
 
                        url(r'^custom_dashboard/theme/$', DashboardThemeList.as_view(), name='dashboard_theme_list'),
-                       url(r'^custom_dashboard/theme_add/$',  DashboardThemeCreate.as_view(), name='dashboard_theme_add'),
-                       url(r'^custom_dashboard/theme_update/(?P<pk>[0-9]+)/$',  DashboardThemeUpdate.as_view(), name='custom_dashboard/theme_update'),
+                       url(r'^custom_dashboard/theme_add/$', DashboardThemeCreate.as_view(), name='dashboard_theme_add'),
+                       url(r'^custom_dashboard/theme_update/(?P<pk>[0-9]+)/$', DashboardThemeUpdate.as_view(), name='custom_dashboard/theme_update'),
                        url(r'^custom_dashboard/theme_delete/(?P<pk>[0-9]+)/$', DashboardThemeDelete.as_view(), name='custom_dashboard/theme_delete'),
                       
                        url(r'^custom_dashboard/component/(?P<pk>[0-9]+)/$', DashboardComponentList.as_view(), name='dashboard_component_list'),                       
