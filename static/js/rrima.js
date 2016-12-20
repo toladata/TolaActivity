fb35740dac2aa6619fda968acf6e42526dc1a427
Kelly Yamamoto <minedamnesia@gmail.com>
2016-12-20 10:55:13 -0800

    put rrima infographic javascript into a separate script and reference in a template

---
+++ static/js/rrima.js
@@ -0,0 +1,7 @@
+// RRIMA CAROUSEL
+$(document).ready(function(ev){
+    $('#custom_carousel').on('slide.bs.carousel', function (evt) {
+      $('#custom_carousel .controls li.active').removeClass('active');
+      $('#custom_carousel .controls li:eq('+$(evt.relatedTarget).index()+')').addClass('active');
+    })
+});
