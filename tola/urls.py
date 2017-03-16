2c6e670bb7b6e8ec112017adfc3532a4661871cd
odenypeter <odenypeter@gmail.com>
2017-03-16 12:02:06 +0300

    merge branch

--- tola/urls.py
+++ tola/urls.py
@@ -114,8 +114,3 @@ urlpatterns = [ # rest framework
 
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
