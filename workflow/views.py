toladata/TolaActivity

commit 34e1a2a106ad78c88161860a35d53cfbc2ab154a

#619: Ensure that PT update does not update PI form

workflow/views.py
@@ -618,31 +618,32 @@ class ProjectCompleteUpdate(UpdateView):
     def get_context_data(self, **kwargs):
         context = super(ProjectCompleteUpdate, self).get_context_data(**kwargs)
         getComplete = ProjectComplete.objects.get(id=self.kwargs['pk'])
-        id = getComplete.project_agreement_id
+        #id = getComplete.project_agreement_id
 
-        context.update({'id': id})
+        context.update({'id': getComplete.pk})
         context.update({'p_name': getComplete.project_name})
         context.update({'p_complete_program': getComplete.program})
         pk = self.kwargs['pk']
         context.update({'pk': pk})
+        context.update({'project_id':getComplete.project_agreement_id})
 
         # get budget data
         try:
-            getBudget = Budget.objects.all().filter(Q(agreement__id=getComplete.project_agreement_id) | Q(complete__id=getComplete.pk) )
+            getBudget = Budget.objects.all().filter(Q(agreement__id=getComplete.project_agreement_id) | Q(complete__id=getComplete.pk))
         except Budget.DoesNotExist:
             getBudget = None
         context.update({'getBudget': getBudget})
 
         # get Quantitative data
         try:
-            getQuantitative = CollectedData.objects.all().filter(agreement__id=getComplete.project_agreement_id).order_by('indicator')
+            getQuantitative = CollectedData.objects.all().filter(Q(agreement__id=getComplete.project_agreement_id) | Q(complete__id=getComplete.pk)).order_by('indicator')
         except CollectedData.DoesNotExist:
             getQuantitative = None
         context.update({'getQuantitative': getQuantitative})
 
         # get benchmark or project components
         try:
-            getBenchmark = Benchmarks.objects.all().filter(agreement__id=getComplete.project_agreement_id).order_by('description')
+            getBenchmark = Benchmarks.objects.all().filter(Q(agreement__id=getComplete.project_agreement_id) | Q(complete__id=getComplete.pk)).order_by('description')
         except Benchmarks.DoesNotExist:
             getBenchmark = None
         context.update({'getBenchmark': getBenchmark})

templates/workflow/projectcomplete_form.html
@@ -2,7 +2,7 @@
 {% block bread_crumb %}
 <ol class="breadcrumb">
   <li><a href="/workflow/dashboard/0/">Program Index</a></li>
-  {% if id %}<li><a href="/workflow/dashboard/project/{{ id }}/">Project Dashboard</a></li>{% endif %}
+  {% if id %}<li><a href="/workflow/dashboard/project/{{ project_id }}/">Project Dashboard</a></li>{% endif %}
   <li class="active">Project Tracking Form </li>
 </ol>
 {%  endblock %}

workflow/forms.py
@@ -836,7 +836,7 @@ class ProjectCompleteForm(forms.ModelForm):
                                   </table>
                               {% endif %}
                               <div class="panel-footer">
-                                <a class="benchmarks" data-toggle="modal" data-target="#myModal" href="/workflow/benchmark_complete_add/{{ id }}/">Add Component</a>
+                                <a class="benchmarks" data-toggle="modal" data-target="#myModal" href="/workflow/benchmark_complete_add/{{ id }}/?is_it_project_complete_form=true">Add Component</a>
                               </div>
                             </div>
 
@@ -883,7 +883,7 @@ class ProjectCompleteForm(forms.ModelForm):
                                       </tbody>
                                       </table>
                                       <div class="panel-footer">
-                                        <a class="output" data-toggle="modal" data-target="#myModal" href="/workflow/budget_add/{{ id }}">Add Budget Contribution</a>
+                                        <a class="output" data-toggle="modal" data-target="#myModal" href="/workflow/budget_add/{{ id }}/?is_it_project_complete_form=true">Add Budget Contribution</a>
                                       </div>
                                     </div>
                                 """),
@@ -920,7 +920,7 @@ class ProjectCompleteForm(forms.ModelForm):
                                           </table>
                                       {% endif %}
                                       <div class="panel-footer">
-                                        <a class="output" data-toggle="modal" data-target="#myModal" href="/workflow/quantitative_add/{{ id }}">Add Quantitative Outputs</a>
+                                        <a class="output" data-toggle="modal" data-target="#myModal" href="/workflow/quantitative_add/{{ id }}/?is_it_project_complete_form=true">Add Quantitative Outputs</a>
                                       </div>
                                     </div>
                              """),
@@ -1103,7 +1103,7 @@ class ProjectCompleteSimpleForm(forms.ModelForm):
                                     </table>
                                 {% endif %}
                                 <div class="panel-footer">
-                                    <a class="benchmarks" data-toggle="modal" data-target="#myModal" href="/workflow/benchmark_complete_add/{{ id }}/" id="btn_bench">Add Component</a>
+                                    <a class="benchmarks" data-toggle="modal" data-target="#myModal" href="/workflow/benchmark_complete_add/{{ id }}/?is_it_project_complete_form=true" id="btn_bench">Add Component</a>
                                 </div>
                             </div>
                         """),
@@ -1172,7 +1172,7 @@ class ProjectCompleteSimpleForm(forms.ModelForm):
                                         </table>
                                     {% endif %}
                                     <div class="panel-footer">
-                                        <a class="output" data-toggle="modal" data-target="#myModal" href="/workflow/quantitative_add/{{ id }}">Add Indicators</a>
+                                        <a class="output" data-toggle="modal" data-target="#myModal" href="/workflow/quantitative_add/{{ id }}/?is_it_project_complete_form=true">Add Indicators</a>
                                     </div>
                                 </div>
                             """),

commit e192b0b254a339bb7861dc8b2932305c754672a4

#668 fixing the bug that was introduced in 34e1a2a106ad78c88161860a35d53cfbc2ab154a

workflow/views.py
@@ -700,7 +700,7 @@ class ProjectCompleteUpdate(UpdateView):
 
     form_class = ProjectCompleteForm
 
-
+import operator
 class ProjectCompleteDetail(DetailView):
 
     model = ProjectComplete
@@ -720,11 +720,17 @@ class ProjectCompleteDetail(DetailView):
         context.update({'id':self.kwargs['pk']})
 
         try:
-            getBenchmark = Benchmarks.objects.filter(Q(agreement__id=self.kwargs['pk']) | Q(complete__id=self.get_object().pk))
+            q_list = [Q(agreement__id=self.kwargs['pk'])]
+            if self.get_object():
+                q_list.append(Q(complete__id=self.get_object().pk))
+            getBenchmark = Benchmarks.objects.filter(reduce(operator.or_, q_list))
         except Benchmarks.DoesNotExist:
             getBenchmark = None
 
-        budgetContribs = getBudget = Budget.objects.filter(Q(agreement__id=self.kwargs['pk']) | Q(complete__id=self.get_object().pk))
+        q_list = [Q(agreement__id=self.kwargs['pk']) ]
+        if self.get_object():
+            q_list.append( Q(complete__id=self.get_object().pk))
+        budgetContribs = Budget.objects.filter(reduce(operator.or_, q_list))
 
         context['budgetContribs'] = budgetContribs
         context['getBenchmarks'] =  getBenchmark