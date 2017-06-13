54bdb00bc274582bb452cf96b158cee541ef9c77
Mahmood Khan <mkhan@mercycorps.org>
2017-06-13 10:47:28 -0700

    on my machine

--- __init__.py
+++ __init__.py
@@ -1,5 +1,1 @@
 __author__ = 'glind'
-# Fix: email is not unique.
-# from django.contrib.auth.models import User
-# User._meta.local_fields[1].__dict__['max_length'] = 75
-# User._meta.get_field('email')._unique = True
