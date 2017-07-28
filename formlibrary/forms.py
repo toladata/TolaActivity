1c24fe7d0ec988581973683f5799d533c0cb3316
Greg Lind <gwlind@gmail.com>
2017-07-28 10:28:10 +0200

    update training form

--- formlibrary/forms.py
+++ formlibrary/forms.py
@@ -35,7 +35,151 @@ class TrainingAttendanceForm(forms.ModelForm):
         self.helper.error_text_inline = True
         self.helper.help_text_inline = True
         self.helper.html5_required = True
-        self.helper.add_input(Submit('submit', 'Save'))
+
+        self.helper.layout = Layout(
+
+            HTML("""<br/>"""),
+            TabHolder(
+                Tab('Details',
+                    Fieldset('',
+                             'training_name',
+                             'workflowlevel1',
+                             'workflowlevel2',
+                             'training_indicator',
+                             'implementer',
+                             'reporting_period',
+                             'start_date',
+                             'end_date',
+                             ),
+                    ),
+                Tab('Location',
+                    Fieldset('',
+                         'location',
+                         'community',
+                         'total_participants',
+                         'input_type_distributed',
+                         'training_duration',
+                         'distributor_contact_number',
+
+                         ),
+                    ),
+                Tab('Information',
+                    Fieldset('',
+                         'form_filled_by',
+                         'form_filled_by_contact_num',
+                         'trainer_name',
+                         'trainer_contact_num',
+                             ),
+                    ),
+            ),
+            HTML("""<br/>"""),
+
+
+                Div(
+                        "",
+                        HTML("""<br/>
+                                {% if getDisaggregationLabelStandard and not getDisaggregationValueStandard %}
+                                    <div class='panel panel-default'>
+                                        <!-- Default panel contents -->
+                                        <div class='panel-heading'>Standard Disaggregations</div>
+                                          <!-- Table -->
+                                          <table class="table">
+                                            <tr>
+                                            <th>Disaggregation Level</th>
+                                            <th>Actuals</th>
+                                            </tr>
+                                            {% for item in getDisaggregationLabelStandard %}
+                                            <tr>
+                                                <td>{{ item.label }}</td>
+                                                <td><input type="text" name="{{ item.id }}" value=""></td>
+                                            </tr>
+                                            {% endfor %}
+                                          </table>
+                                    </div>
+                                {% else %}
+                                    {% if not getDisaggregationValueStandard %}
+                                        <h4>Standard Disaggregation Levels Not Entered</h4>
+                                        <p>Standard disaggregations are entered in the administrator for the entire organizations.  If you are not seeing
+                                        any here, please contact your system administrator.</p>
+                                    {% endif %}
+                                {% endif %}
+                                {% if getDisaggregationLabel and not getDisaggregationValue %}
+                                    <div class='panel panel-default'>
+                                        <!-- Default panel contents -->
+                                        <div class='panel-heading'>New Disaggregations</div>
+                                          <!-- Table -->
+                                          <table class="table">
+                                            <tr>
+                                            <th>Disaggregation Level</th>
+                                            <th>Actuals</th>
+                                            </tr>
+                                            {% for item in getDisaggregationLabel %}
+                                            <tr>
+                                                <td>{{ item.label }}</td>
+                                                <td><input type="text" name="{{ item.id }}" value=""></td>
+                                            </tr>
+                                            {% endfor %}
+                                          </table>
+                                    </div>
+                                {% else %}
+                                    {% if not getDisaggregationValue %}
+                                        <h4>Disaggregation Levels Not Entered For This Indicator</h4>
+                                        <a href="/indicators/indicator_update/{{ indicator_id }}">Add a Disaggregation</a>
+                                    {% endif %}
+                                {% endif %}
+
+                                {% if getDisaggregationValue %}
+                                    <div class='panel panel-default'>
+                                        <!-- Default panel contents -->
+                                        <div class='panel-heading'>Existing Disaggregations</div>
+
+                                          <!-- Table -->
+                                          <table class="table">
+                                            <tr>
+                                            <th>Disaggregation Level</th>
+                                            <th>Actuals</th>
+                                            </tr>
+                                            {% for item in getDisaggregationValue %}
+                                            <tr>
+                                                <td>{{ item.disaggregation_label.label }}</td>
+                                                <td><input type="text" name="{{ item.disaggregation_label.id }}" value="{{ item.value }}"></td>
+                                            </tr>
+                                            {% endfor %}
+                                          </table>
+
+                                    </div>
+                                {% endif %}
+
+                                {% if getDisaggregationValueStandard %}
+                                    <div class='panel panel-default'>
+                                        <!-- Default panel contents -->
+                                        <div class='panel-heading'>Existing Standard Disaggregations</div>
+
+                                          <!-- Table -->
+                                          <table class="table">
+                                            <tr>
+                                            <th>Disaggregation Level</th>
+                                            <th>Actuals</th>
+                                            </tr>
+                                            {% for item in getDisaggregationValueStandard %}
+                                            <tr>
+                                                <td>{{ item.disaggregation_label.label }}</td>
+                                                <td><input type="text" name="{{ item.disaggregation_label.id }}" value="{{ item.value }}"></td>
+                                            </tr>
+                                            {% endfor %}
+                                          </table>
+
+                                    </div>
+                                {% endif %}
+                             """),
+                ),
+
+            HTML("""<br/>"""),
+            FormActions(
+                Submit('submit', 'Save', css_class='btn-default'),
+                Reset('reset', 'Reset', css_class='btn-warning')
+            )
+        )
 
         super(TrainingAttendanceForm, self).__init__(*args, **kwargs)
 
