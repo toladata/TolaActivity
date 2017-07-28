b74f5789cad9088310fa19b7f0ead5be66b6a0a9
Greg Lind <gwlind@gmail.com>
2017-07-28 14:56:56 +0200

    update indicator reports

--- scripts/kpi.py
+++ scripts/kpi.py
@@ -23,13 +23,13 @@ from indicators.models import Indicator, Level
 
 for program in WorkflowLevel1.objects.all():
     kpi_count = 0
-    for indicator in Indicator.objects.all().filter(program__id=program.id):
+    for indicator in Indicator.objects.all().filter(workflowlevel1__id=program.id):
         if indicator.key_performance_indicator == True:
             kpi_count = kpi_count + 1
     # if the program has no kpis update goal level indicators to be kpi
     if kpi_count == 0:
         # get_level = Level.objects.get(name="Goal")
         get_level = Level.objects.get(name="Impact")
-        Indicator.objects.all().filter(program__id=program.id,level=get_level).update(key_performance_indicator=True)
+        Indicator.objects.all().filter(workflowlevel1__id=program.id,level=get_level).update(key_performance_indicator=True)
     print program
     print kpi_count
