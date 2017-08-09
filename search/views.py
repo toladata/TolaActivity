a78d0d80501389f6430d65c7f4eab18e6dcb1648
leon <git@leonholub.com>
2017-08-09 12:21:16 +0200

    Removed unused import

--- search/views.py
+++ search/views.py
@@ -4,8 +4,6 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.core.management import call_command
 
-from django.shortcuts import render
-
 @login_required(login_url='/accounts/login/')
 def search_index(request):
     call_command('search-index', '_all')
