3329d0f5e4f2466190e6985c7c3556a2c21e0154
Greg Lind <gwlind@gmail.com>
2017-09-22 16:47:04 +0200

    update migration file to use 1

--- indicators/migrations/0020_auto_20170919_0648.py
+++ indicators/migrations/0020_auto_20170919_0648.py
@@ -26,6 +26,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='disaggregationtype',
             name='organization',
-            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, to='workflow.Organization'),
+            field=models.ForeignKey(default=1, on_delete=django.db.models.deletion.CASCADE, to='workflow.Organization'),
         ),
     ]
