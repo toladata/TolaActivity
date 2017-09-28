59050c66c103abd71416bab4f8721e986d007de3
Greg Lind <gwlind@gmail.com>
2017-09-28 14:31:30 +0200

    workflowlevel1 filter for team

--- feed/views.py
+++ feed/views.py
@@ -989,8 +989,8 @@ class WorkflowTeamViewSet(viewsets.ModelViewSet):
         if request.user.is_superuser:
             queryset = WorkflowTeam.objects.all()
         else:
-            user_org = TolaUser.objects.get(user=request.user).organization
-            queryset = WorkflowTeam.objects.all().filter(organization=user_org)
+            user_level1 = getLevel1(request.user)
+            queryset = WorkflowTeam.objects.all().filter(workflowlevel1__in=user_level1)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
