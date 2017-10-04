46b69b5f52f8bdebdffc42a113fd1d329908d5c0
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2017-10-04 15:28:54 +0200

    Use Django conf if set and update Dev conf

--- manage.py
+++ manage.py
@@ -4,11 +4,15 @@ import sys
 
 if __name__ == "__main__":
 
-    settings = 'tola.settings.test' if 'test' in sys.argv else 'tola.settings.local'
+    if 'test' in sys.argv:
+        settings = 'tola.settings.test'
+    elif os.environ.get("DJANGO_SETTINGS_MODULE"):
+        settings = os.environ.get("DJANGO_SETTINGS_MODULE")
+    else:
+        settings = 'tola.settings.local'
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings)
 
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
-
