b3cfb673868e06eb77a497f234f489557034185f
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2017-10-05 14:43:14 +0200

    Remove deprecated TEMPLATE_DEBUG
    Refer: https://docs.djangoproject.com/en/1.11/ref/templates/upgrading/#the-templates-settings

--- tola/settings/test.py
+++ tola/settings/test.py
@@ -34,11 +34,6 @@ MANAGERS = ADMINS
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
 
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#template-debug
-TEMPLATE_DEBUG = DEBUG
-########## END DEBUG CONFIGURATION
-
-
 ########## EMAIL CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
