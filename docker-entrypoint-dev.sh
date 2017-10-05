183af1682b2cf554e4ad2d8afcc0e55cb4f5bb15
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2017-10-05 14:15:41 +0200

    Load config fixtures on docker entrypoint

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -10,5 +10,8 @@ python manage.py makemigrations
 echo "Creating admin user"
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(email='admin@humanitec.com').delete(); User.objects.create_superuser('admin', 'admin@humanitec.com', 'admin')"
 
+echo "Loading config fixtures"
+python manage.py loaddata fixtures/config/*
+
 echo "Running the server"
 python manage.py runserver 0.0.0.0:8000
