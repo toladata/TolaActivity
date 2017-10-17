c40d92f8681d6bee4a6bebdf098a3b3fae001940
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2017-10-17 10:22:30 +0200

    Add user to multiple properties in Program #348

--- feed/tests/test_workflowlevel1view.py
+++ feed/tests/test_workflowlevel1view.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from rest_framework.test import APIRequestFactory
 
 from feed.views import WorkflowLevel1ViewSet, ROLE_PROGRAM_ADMIN
-from workflow.models import Organization, TolaUser, WorkflowTeam
+from workflow.models import Organization, TolaUser, WorkflowTeam, WorkflowLevel1
 
 
 class WorkflowLevel1ViewsTest(TestCase):
@@ -12,8 +12,8 @@ class WorkflowLevel1ViewsTest(TestCase):
         self.user.is_superuser = True
         self.user.is_staff = True
         self.user.save()
-        organization = Organization.objects.create(name="TestOrg")
-        TolaUser.objects.create(user=self.user, organization=organization)
+        self.organization = Organization.objects.create(name="TestOrg")
+        TolaUser.objects.create(user=self.user, organization=self.organization)
         Group.objects.create(name=ROLE_PROGRAM_ADMIN)
 
     def test_create_workflowlevel1(self):
@@ -27,3 +27,7 @@ class WorkflowLevel1ViewsTest(TestCase):
         self.assertEqual(response.data['name'], u'Save the Children')
         wft = WorkflowTeam.objects.get(workflowlevel1__id=response.data['id'])
         self.assertEqual(wft.workflow_user, self.user.tola_user)
+        wflvl1 = WorkflowLevel1.objects.get(pk=response.data['id'])
+        self.assertEqual(wflvl1.organization, self.organization)
+        self.assertEqual(wflvl1.user_access.all().count(), 1)
+        self.assertEqual(wflvl1.user_access.first(), self.user.tola_user)
