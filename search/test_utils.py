aaabd4e56d28666f9a3abaf8ae5c038af0252b5c
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2017-10-25 14:59:31 +0200

    Skip Elasticsearch tests if config not found
    Drone test builds for Elasticsearch could not be setup, so there
    is no more option than skipping tests.

--- search/test_utils.py
+++ search/test_utils.py
@@ -1,3 +1,6 @@
+from unittest import skipIf
+
+from django.conf import settings
 from django.test import TestCase
 
 from indicators.models import Indicator
@@ -5,10 +8,12 @@ from search.exceptions import ValueNotFoundError
 from search.utils import ElasticsearchIndexer
 
 
+@skipIf(not settings.ELASTICSEARCH_URL, "Elasticsearch config not found")
 class ElasticsearchIndexerTest(TestCase):
     def test_es_connection(self):
         indexer = ElasticsearchIndexer()
-        self.assertTrue(indexer.es.ping(), "Cannot connect to Elasticsearch")
+        self.assertTrue(indexer.es.ping(),
+                        "Cannot connect to Elasticsearch. URL: {}".format(settings.ELASTICSEARCH_URL))
 
     def test_create_and_delete_indicator(self):
         indexer = ElasticsearchIndexer()
