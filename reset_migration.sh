34436c40adc1f4646b81d885de62c020cf63fd27
Jeferson Moura <jefmmoreira@gmail.com>
2017-10-26 14:47:21 +0200

    Add missing commands to reset migration script

--- reset_migration.sh
+++ reset_migration.sh
@@ -7,5 +7,12 @@ python manage.py migrate --fake indicators zero
 python manage.py migrate --fake reports zero
 python manage.py migrate --fake workflow zero
 
+echo "#### Delete old migration scripts ####"
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+
+echo "#### Create the initial migrations ####"
+python manage.py makemigrations
+
 echo "#### Fake the initial migration ####"
 python manage.py migrate --fake-initial
