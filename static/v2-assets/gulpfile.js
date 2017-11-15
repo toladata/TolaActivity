3ceedfd14dc4b1c3ad775923946c6b63ec0b73dc
Mateusz Jurzyna <jurzyna@gmail.com>
2017-11-15 09:56:31 +0100

    Added the right js files #806 #807

--- static/v2-assets/gulpfile.js
+++ static/v2-assets/gulpfile.js
@@ -29,7 +29,7 @@ gulp.task('sass', function () {
 
 // Move the javascript files into our /src/js folder
 gulp.task('js', function() {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/tether/dist/js/tether.min.js'])
+    return gulp.src(['node_modules/jquery/dist/jquery.min.js','node_modules/bootstrap/dist/js/bootstrap.bundle.min.js', 'src/js/main.js'])
         .pipe(concat('script.js'))
         .pipe(gulp.dest("src/js"))
         .pipe(browserSync.stream());
