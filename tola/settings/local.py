edc6dd26d76e23bd48e21b9350ec7d27490acb94
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2017-11-20 16:48:32 +0100

    PEP8

--- tola/settings/local.py
+++ tola/settings/local.py
@@ -1,5 +1,6 @@
 from base import *
 import os
+from os.path import join, normpath
 
 try:
     DATABASES = {
@@ -29,9 +30,6 @@ if os.getenv('TOLA_HOSTNAME') is not None:
 
 USE_X_FORWARDED_HOST = True if os.getenv('TOLA_USE_X_FORWARDED_HOST') == 'True' else False
 
-
-from os.path import join, normpath
-
 ########## MANAGER CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#admins
 ADMINS = (
@@ -47,13 +45,6 @@ MANAGERS = ADMINS
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True if os.getenv('TOLA_DEBUG') == 'True' else False
 
-########## EMAIL CONFIGURATION
-# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
-########## END EMAIL CONFIGURATION
-
-########## EMAIL SETTINGS
-
-########## END EMAIL SETTINGS
 CORS_ORIGIN_ALLOW_ALL = True
 
 ########## SOCIAL AUTH CLIENT CONFIG ###########
@@ -67,7 +58,7 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET')
 SOCIAL_AUTH_MICROSOFT_GRAPH_KEY = os.getenv('SOCIAL_AUTH_MICROSOFT_GRAPH_KEY')
 SOCIAL_AUTH_MICROSOFT_GRAPH_SECRET = os.getenv('SOCIAL_AUTH_MICROSOFT_GRAPH_SECRET')
 SOCIAL_AUTH_MICROSOFT_GRAPH_REDIRECT_URL = os.getenv('SOCIAL_AUTH_MICROSOFT_GRAPH_REDIRECT_URL')
-SOCIAL_AUTH_FIELDS_STORED_IN_SESSION = ['redirect_after_login',]
+SOCIAL_AUTH_FIELDS_STORED_IN_SESSION = ['redirect_after_login']
 
 
 ########## CACHE CONFIGURATION
@@ -89,8 +80,7 @@ LOCAL_API_TOKEN = "ABC"
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [normpath(join(SITE_ROOT, 'templates2')),],
-        #'APP_DIRS': True,
+        'DIRS': [normpath(join(SITE_ROOT, 'templates2'))],
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.request',
@@ -112,7 +102,6 @@ TEMPLATES = [
         },
     },
 ]
-
 
 LOGGING = {
     'version': 1,
@@ -141,8 +130,3 @@ TOLA_TRACK_TOKEN = os.getenv('TOLA_TRACK_TOKEN')
 
 ELASTICSEARCH_URL = os.getenv('ELASTICSEARCH_URL')
 ELASTICSEARCH_INDEX_PREFIX = os.getenv('ELASTICSEARCH_INDEX_PREFIX')
-
-try:
-    from local_secret import *
-except ImportError:
-    pass
