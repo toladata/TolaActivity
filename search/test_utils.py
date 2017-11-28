9852547173f0efe830d888d1a6195a3228526d41
leon <git@leonholub.com>
2017-11-28 10:18:00 +0100

    Changed tests to only test ES within the search tests.

--- search/test_utils.py
+++ search/test_utils.py
@@ -12,41 +12,44 @@ from search.utils import ElasticsearchIndexer
 
 @skipIf(not settings.ELASTICSEARCH_URL, "Elasticsearch config not found")
 class ElasticsearchIndexerTest(TestCase):
+    indexer = None
+
+    def setUp(self):
+        settings.ELASTICSEARCH_ENABLED = True
+        self.indexer = ElasticsearchIndexer()
+
+    def tearDown(self):
+        settings.ELASTICSEARCH_ENABLED = False
+
     def test_es_connection(self):
-        indexer = ElasticsearchIndexer()
-        self.assertTrue(indexer.es.ping(),
+        self.assertTrue(self.indexer.es.ping(),
                         "Cannot connect to Elasticsearch. URL: {}".format(settings.ELASTICSEARCH_URL))
 
     def test_create_and_delete_indicator(self):
-        indexer = ElasticsearchIndexer()
         indicator = Indicator.objects.create(name="TestIndicator")
-        indexer.delete_indicator(indicator)
+        self.indexer.delete_indicator(indicator)
 
     def test_delete_unexisting_indicator_raises_exception(self):
-        indexer = ElasticsearchIndexer()
         org = Organization.objects.create(organization_uuid="not-existing-uuid")
         wflvl1 = factories.WorkflowLevel1(organization=org)
         indicator = factories.Indicator(workflowlevel1=[wflvl1])
-        indexer.delete_indicator(indicator)
+        self.indexer.delete_indicator(indicator)
 
-        self.assertRaises(ValueNotFoundError, indexer.delete_indicator, indicator)
+        self.assertRaises(ValueNotFoundError, self.indexer.delete_indicator, indicator)
 
     def test_create_and_delete_workflowlevel1(self):
-        indexer = ElasticsearchIndexer()
         org = Organization.objects.create(organization_uuid="index-workflowlevel1-test")
         wflvl1 = factories.WorkflowLevel1(organization=org)
-        indexer.delete_workflowlevel1(wflvl1)
+        self.indexer.delete_workflowlevel1(wflvl1)
 
     def test_create_and_delete_workflowlevel2(self):
-        indexer = ElasticsearchIndexer()
         org = Organization.objects.create(organization_uuid="index-workflowlevel2-test")
         wflvl1 = factories.WorkflowLevel1(organization=org)
         wflvl2 = factories.WorkflowLevel2(workflowlevel1=wflvl1)
-        indexer.delete_workflowlevel2(wflvl2)
+        self.indexer.delete_workflowlevel2(wflvl2)
 
     def test_create_and_delete_collecteddata(self):
-        indexer = ElasticsearchIndexer()
         org = Organization.objects.create(organization_uuid="index-collecteddata-test")
         wflvl1 = factories.WorkflowLevel1(organization=org)
         collecteddata = factories.CollectedData(workflowlevel1=wflvl1)
-        indexer.delete_collecteddata(collecteddata)
+        self.indexer.delete_collecteddata(collecteddata)
