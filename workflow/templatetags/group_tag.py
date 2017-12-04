3ccb547231add0c4b1fd2fc6b590ac4dcf635f5e
Mahmood Khan <mkhan@mercycorps.org>
2017-12-04 09:05:34 -0800

    udpate footer links for MC only

--- workflow/templatetags/group_tag.py
+++ workflow/templatetags/group_tag.py
@@ -1,6 +1,9 @@
 from django import template
 from django.contrib.auth.models import Group
 
+from django import template
+from django.template.defaultfilters import stringfilter
+
 register = template.Library()
 
 
@@ -8,3 +11,13 @@ register = template.Library()
 def has_group(user, group_name):
     group = Group.objects.get(name=group_name)
     return True if group in user.groups.all() else False
+
+
+@register.filter
+@stringfilter
+def template_exists(template_name):
+    try:
+        template.loader.get_template("links.html")
+        return True
+    except template.TemplateDoesNotExist:
+        return False
