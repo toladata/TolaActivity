d151e9c17536d25db4098d428a73ff55865508ad
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2017-12-06 15:13:07 +0100

    Do not generate migration files on Dev entrypoint

--- docker-entrypoint-dev.sh
+++ docker-entrypoint-dev.sh
@@ -5,7 +5,6 @@ python manage.py collectstatic -v 0 --noinput
 
 echo "Migrate"
 python manage.py migrate
-python manage.py makemigrations
 
 echo "Creating admin user"
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(email='admin@humanitec.com').delete(); User.objects.create_superuser('admin', 'admin@humanitec.com', 'admin')"
