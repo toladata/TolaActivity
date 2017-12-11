260b6a286a928e5aa963f7173723bb21a58cdb78
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2017-12-11 11:39:46 +0100

    Fallback to user's name when printing TolaUser instance

--- workflow/test_models.py
+++ workflow/test_models.py
@@ -16,7 +16,29 @@ class TolaUserTest(TestCase):
         self.assertEqual(tola_user.name, 'Fake Name')
 
     def test_save_without_ofuscate_name(self):
-
         tola_user = TolaUser.objects.create(name='Thom Yorke', user=self.user,
                                             organization=self.organization)
         self.assertEqual(tola_user.name, 'Thom Yorke')
+
+    def test_print_instance_no_name(self):
+        tolauser = factories.TolaUser(name=None)
+        self.assertEqual(unicode(tolauser), u'-')
+
+    def test_print_instance_empty_name(self):
+        tolauser = factories.TolaUser(name='')
+        self.assertEqual(unicode(tolauser), u'-')
+
+    def test_print_instance_name(self):
+        tolauser = factories.TolaUser(name='Daniel Avery')
+        self.assertEqual(unicode(tolauser), u'Daniel Avery')
+
+    @override_settings(TOLAUSER_OFUSCATED_NAME='Fake Name')
+    def test_print_instance_with_ofuscated_name_and_fallback(self):
+        tolauser = factories.TolaUser()
+        self.assertEqual(unicode(tolauser), u'Thom Yorke')
+
+    @override_settings(TOLAUSER_OFUSCATED_NAME='Fake Name')
+    def test_print_instance_with_ofuscated_name_without_fallback(self):
+        user = factories.User(first_name='', last_name='')
+        tolauser = factories.TolaUser(user=user)
+        self.assertEqual(unicode(tolauser), u'-')
