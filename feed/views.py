70bfaeeff51cd3561b1b01e68d4197a61bf1c7e0
Rafael Muñoz Cárdenas <rafael@humanitec.com>
2018-01-19 11:53:50 +0100

    Delete non used LargeResultsSetPagination

--- feed/views.py
+++ feed/views.py
@@ -26,12 +26,6 @@ from .permissions import IsOrgMember, AllowTolaRoles
 from . import serializers
 
 
-class LargeResultsSetPagination(PageNumberPagination):
-    page_size = 1000
-    page_size_query_param = 'page_size'
-    max_page_size = 10000
-
-
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = 'page_size'
