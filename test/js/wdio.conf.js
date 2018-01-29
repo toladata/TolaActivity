86614f7090cd15a5969a938cc72a652ab870b525
Kurt Wall <kwall@kurtwerks.com>
2018-01-28 22:22:50 -0800

    Remove unused packages; default to spec reporter

--- test/js/wdio.conf.js
+++ test/js/wdio.conf.js
@@ -122,8 +122,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: http://webdriver.io/guide/reporters/dot.html
-    reporters: ['spec','dot'],
-    
+    reporters: ['spec'],
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
