6cdc227a53d3d3d5bcf1349c53244accb3b63016
leon <git@leonholub.com>
2018-02-14 13:43:22 +0100

    Cleaned up imports and unused urls in the search module

--- search/urls.py
+++ search/urls.py
@@ -1,11 +1,6 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
 from .views import *
-from indicators import views as indicatorviews
-
-
 
 urlpatterns = [
-    #url(r'^update_index', search_index, name='search_index'),
     url(r'^get/(?P<index>\w+)/(?P<term>\w+)', search),
-
 ]
