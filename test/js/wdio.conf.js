8023dafe2c7e0911ce14a3ed36de30b9c8fa0032
Kurt Wall <kwall@kurtwerks.com>
2018-02-20 09:21:15 -0800

    Re-enable the chrome "capability"

--- test/js/wdio.conf.js
+++ test/js/wdio.conf.js
@@ -42,10 +42,10 @@ exports.config = {
     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
     // grid with only 5 chrome instances available you can make sure that not more than
     // 5 instances get started at a time.
-//        maxInstances: 1,
-//        browserName: 'chrome'
-//    },
-//    {
+        maxInstances: 1,
+        browserName: 'chrome'
+    },
+    {
         maxinstances: 1,
         browserName: 'firefox'
     }],
