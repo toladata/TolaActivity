cdda9ddf91312eeef75ea384a00695098f8e81df
Kurt Wall <kwall@kurtwerks.com>
2018-02-26 00:06:05 -0800

    Break huge indicators.js file into smaller ones.

--- test/js/test/specs/indicators.js
+++ test/js/test/specs/indicators.js
@@ -12,212 +12,94 @@ describe('Program Indicators page', function() {
     // Disable timeouts
     this.timeout(0);
     browser.windowHandleMaximize();
-  });
-
-  it('should exist', function() {
     let parms = util.readConfig();
     LoginPage.open(parms.baseurl);
     LoginPage.setUserName(parms.username);
     LoginPage.setPassword(parms.password);
     LoginPage.clickLoginButton();
+  });
+
+  it('should exist', function() {
     IndPage.open();
     // FIXME: pageName should be a property
     assert.equal('Program Indicators', IndPage.pageName());
   });
 
-  // FIXME: Still need to get WebDriver code out of this test
-  it('should toggle PIs table by clicking PI Indicators button', function() {
-    if(browser.isVisible('div#ajaxloading')) {
-      browser.waitForVisible('div#ajaxloading', delay, true);
-    }
-    IndPage.clickIndicatorsLink();
-    let buttons = TargetsTab.getProgramIndicatorButtons();
-    for (let button of buttons) {
-      let targetDiv = 'div' + button.getAttribute('data-target');
-      let isVisible = browser.isVisible(targetDiv);
-      // Starts out collapsed
-      assert.equal(false, isVisible);
+  describe('Create an Indicator form', function() {
+    it('should show context-sensitve help by clicking Form Help/Guidance button');
+    it('should have an Indicator Service Templates dropdown');
+    it('should have a Custom entry in the Indicator Service Templates dropdown');
+    it('should have a Service Indicator dropdown');
+    it('should have a Country dropdown');
+    it('should have a Program dropdown');
+    it('should have a Save button');
+    it('should validate input data after clicking Save button and before committing it');
+    it('should not save unvalidated input data');
+    it('should highlight invalid input data');
+    it('should save data after validating it');
+    it('should open Indicator detail form after clicking Save button');
+    it('should have a Cancel button');
+    it('should reset and close form when Cancel button clicked');
+    it('should return to previous screen when Cancel button clicked');
+    it('should have a Reset button to reset form');
+    it('should have a Summary tab');
+    it('should have a Performance tab');
+    it('should hnve a Targets tab');
+    it('should have a Data Acquisition tab');
+    it('should have a Analysis and Reporting tab');
+    it('should have an Approval tab');
+    it('should have a Help link');
+    it('should have a Save Changes button');
+    it('should have a Reset button');
+    it('should have a Cancel button');
+    it('should trigger cancel action by pressing Escape key');
+    it('should validate input data after clicking Save Changes button');
+    it('should validate input data before committing it');
+    it('should restore form to pre-edit state when Reset button is clicked');
 
-      // Open it and verify
-      button.click();
-      if(browser.isVisible('div#ajaxloading')) {
-        browser.waitForVisible('div#ajaxloading', delay, true);
-      }
-      isVisible = browser.isVisible(targetDiv);
-      assert.equal(true, isVisible);
-
-      button.click();
-      if(browser.isVisible('div#ajaxloading')) {
-        browser.waitForVisible('div#ajaxloading', delay, true);
-      }
-    }
-  });
-
-  describe('Program Indicators', function() {
-    it('should be viewable by clicking their names in Indicator Name column', function() {
-      if (browser.isVisible('div#ajaxloading')) {
-        browser.waitForVisible('div#ajaxloading', delay, true);
-      }
-      IndPage.clickIndicatorsLink();
-      // Make list of Indicators buttons
-      let buttons = TargetsTab.getProgramIndicatorButtons();
-      // Click the first one to expand the table
-      let button = buttons[0];
-      button.click();
-      // Make list of indicator names in resulting table
-      // FIXME: needs to be from table, not dropdown
-      let indicatorNameList = IndPage.getIndicatorsDropdownList();
-      // Click the first one
-      if (browser.isVisible('div#ajaxloading')) {
-        browser.waitForVisible('div#ajaxloading', delay, true);
-      }
-      let indicatorName = indicatorNameList[0];
-      TargetsTab.clickProgramIndicatorsButton(indicatorName);
-    });
-
-    it('should be able to create PI by clicking the New Indicator button', function() {
-      TargetsTab.clickNewIndicatorButton();
-      TargetsTab.saveNewIndicator();
-      TargetsTab.setIndicatorName('New Indicator button test');
-      TargetsTab.setUnitOfMeasure('Bugs fixed');
-      TargetsTab.setLoPTarget(172);
-      TargetsTab.setBaseline(173);
-      TargetsTab.setTargetFrequency('Life of Program (LoP) only');
-      TargetsTab.saveIndicatorChanges();
-    });
-
-    it('should increase PI count after adding new indicator');
-    it('should be able to delete PI by clicking its Delete button');
-    it('should decrease PI count after deleting indicator');
-    it('should be able to edit PI by clicking its Edit button');
-    it('should open the Grid/Print Report page when button is clicked');
-    it('should highlight invalid data');
-    it('should return to previous screen if Cancel button clicked');
-    it('should clear form when Clear button clicked');
-
-    describe('Create an Indicator form', function() {
-      it('should show context-sensitve help by clicking Form Help/Guidance button');
-      it('should have an Indicator Service Templates dropdown');
-      it('should have a Custom entry in the Indicator Service Templates dropdown');
-      it('should have a Service Indicator dropdown');
-      it('should have a Country dropdown');
-      it('should have a Program dropdown');
-      it('should have a Save button');
-      it('should validate input data after clicking Save button and before committing it');
-      it('should not save unvalidated input data');
-      it('should highlight invalid input data');
-      it('should save data after validating it');
-      it('should open Indicator detail form after clicking Save button');
-      it('should have a Cancel button');
-      it('should reset and close form when Cancel button clicked');
-      it('should return to previous screen when Cancel button clicked');
-      it('should have a Reset button to reset form');
-      it('should have a Summary tab');
-      it('should have a Performance tab');
-      it('should hnve a Targets tab');
-      it('should have a Data Acquisition tab');
-      it('should have a Analysis and Reporting tab');
-      it('should have an Approval tab');
-      it('should have a Help link');
-      it('should have a Save Changes button');
-      it('should have a Reset button');
-      it('should have a Cancel button');
-      it('should trigger cancel action by pressing Escape key');
-      it('should validate input data after clicking Save Changes button');
-      it('should validate input data before committing it');
-      it('should restore form to pre-edit state when Reset button is clicked');
-
-      describe('Indicator creation detail form', function() {
-        it.skip('should confirm indicator created', function() {
-          // CSS selector: #alerts
-          // CSS path: .alert
-          // XPath: /html/body/div[1]/div[2]/div
-          el = driver.findElement({xpath: '/html/body/div[1]/div[2]/div'}).then(function(el) {
-            assert.include(el.getText(), 'Success', el.getText());
-            assert.match(el.getText(), '/^Success .*Created$/', el.getText());
-          });
+    describe('Indicator creation detail form', function() {
+      it.skip('should confirm indicator created', function() {
+        // CSS selector: #alerts
+        // CSS path: .alert
+        // XPath: /html/body/div[1]/div[2]/div
+        el = driver.findElement({xpath: '/html/body/div[1]/div[2]/div'}).then(function(el) {
+          assert.include(el.getText(), 'Success', el.getText());
+          assert.match(el.getText(), '/^Success .*Created$/', el.getText());
         });
+      });
 
-        describe('Summary tab', function() {
-          it('should exist');
-          it('should have Program field matching input data')
-        }); // end summary tab tests
+      describe('Summary tab', function() {
+        it('should exist');
+        it('should have Program field matching input data')
+      }); // end summary tab tests
 
-        describe('Performance tab', function() {
-          it('should exist');
-        }); // end performance tab tests
+      describe('Performance tab', function() {
+        it('should exist');
+      }); // end performance tab tests
 
-        describe('Targets tab', function() {
-          it('should exist');
-        }); // end targets tab tests
+      describe('Targets tab', function() {
+        it('should exist');
+      }); // end targets tab tests
 
-        describe('Data Acquisition tab', function() {
-          it('should exist');
-        }); // end data acquistion tab tests
+      describe('Data Acquisition tab', function() {
+        it('should exist');
+      }); // end data acquistion tab tests
 
-        describe('Analysis and Reporting tab', function() {
-          it('should exist');
-        }); // end analysis tab tests
+      describe('Analysis and Reporting tab', function() {
+        it('should exist');
+      }); // end analysis tab tests
 
-        describe('Approval tab', function() {
-          it('should exist');
-        }); // end approval tab tests
-      }); // end create indicator detail page tests
-    }); // end create new indicator form tests
+      describe('Approval tab', function() {
+        it('should exist');
+      }); // end approval tab tests
+    }); // end create indicator detail page tests
+  }); // end create new indicator form tests
 
-    describe('Grid/Print Report page', function() {
-      it('should exist');
-      it('should have an Export All button');
-      it('should export all report entries when Export All button is clicked');
-    });
-
-    describe('Indicator evidence dropdown', function() {
-      it('should be able to view PI evidence table by clicking its Data button');
-      it('should decrease evidence count when PI evidence deleted');
-      it('should increase evidence count when PI evidence added');
-      it("should toggle indicator's evidence dropdown by clicking its Data button");
-      it('should have the same row count as evidence count on Data button');
-      it('should be able to edit evidence line item by clicking its Edit button');
-      it('should be able to edit evidence line item by clicking its Indicator Name');
-      it('should open Collected Data form when editing evidence line item');
-      it('should be able to delete evidence line item by clicking its Delete button');
-      it('should be able to add evidence line item by clicking the New Data button');
-      it('should open blank Collected Data form when the New Data button is clicked');
-    });
-
-    describe('Collected Data form', function() {
-      it('should exist');
-      it('should require required fields');
-      it('should not require optional fields');
-      it('should validate input data before saving it');
-      it('should not save invalid input data');
-      it('should have a Reset button');
-      it('should clear the form by clicking the Reset button');
-      it('should restore the form defaults by clicking the Reset button');
-      it('should open the Indicator edit form by clicking the Add a Disaggregation link');
-      it('should only accept numeric values in the disaggregations Actuals fields');
-      it('should have a cancel button');
-      it('should be able to close the form by clicking the Cancel button');
-    });
-
-    // TODO: Indicator Delete confirmation page only has a Confirm
-    // button and no way to indicate "No" except using the browser's
-    // Back button to return to the previous page. Except that operation
-    // actually return the user to in the Program Indicators index page
-    // instead. This violates the Principle of Least Surprise.
-    it('should close the delete dialog by pressing the Esc key');
-    it('should have a Cancel button that returns to the previous screen');
-    it('should identify required fields as such');
-    it('should not permit read-only fields to be edited');
+  describe('Grid/Print Report page', function() {
     it('should have a Grid/Print Report button');
-    it('should open a report page when the Grid/Print Report button is clicked');
+    it('should open when the Grid/Print Report button is clicked');
+    it('should exist');
+    it('should have an Export All button');
+    it('should export all report entries when Export All button is clicked');
   });
-
-  // These are enhancements
-  it('should highlight PIs with no evidence');
-  it('should disable Indicators button if program has no indicators');
-  it('should be able to sort table by clicking a column header');
-  it('should be able to select any/all list items');
-  it('should default to showing all Indicator Types for a program');
-  it('should be able to filter the resultset by Indicator Type');
 });
