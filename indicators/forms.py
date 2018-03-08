569c8bdb9662ca0b7cdd93594e7b5fe8d68e5396
Mahmood Khan <mkhan@mercycorps.org>
2018-03-08 12:45:08 -0800

    use templates to manually render form

--- indicators/forms.py
+++ indicators/forms.py
@@ -232,7 +232,7 @@ class IndicatorForm(forms.ModelForm):
         self.fields['approved_by'].queryset = TolaUser.objects.filter(country__in=countries).distinct()
         self.fields['approval_submitted_by'].queryset = TolaUser.objects.filter(country__in=countries).distinct()
         self.fields['program'].widget.attrs['readonly'] = "readonly"
-        self.fields['baseline'].widget.attrs['class'] = 'col-sm-4'
+        # self.fields['baseline'].widget.attrs['class'] = 'col-sm-4'
         #self.fields['target_frequency_start'].widget = DatePicker.DateInput()
         # self.fields['target_frequency_start'].help_text = 'This field is required'
         # self.fields['target_frequency'].required = False
