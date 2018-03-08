d5348e5abcb79b9a33945cd8340a6eb795d67439
Kurt Wall <kwall@kurtwerks.com>
2018-03-07 17:46:56 -0800

    Create initial test cases for v1.9.5

---
+++ test/js/test/specs/export_reports.js
@@ -0,0 +1,8 @@
+// Proposed test cases for TolaActivity v1.9.5
+
+describe('Export reports', function() {
+  it('should default to unformatted CSV');
+  it('should be able to save as PDF');
+  it('should have a shareable permalink');
+  it('should be able to pin a report to the home page dashboard');
+}):
---
+++ test/js/test/specs/prog_impact_overview.js
@@ -0,0 +1,16 @@
+// Proposed test cases for TolaActivity v1.9.5
+
+describe('Program impact overview', function() {
+  it('should display combinations of indicators with compatible target frequencies');
+  it('should not display combinations of indicators with incompatible target frequencies');
+  it('should display Targets and % Met fields in the report');
+  it('should have Program dropdown');
+  it('should have a Time Span dropdown');
+  it('should have a Start date field');
+  it('should have an End date field');
+  it('should require choosing a program');
+  it('should require choosing a time span');
+  it('should require a Start date');
+  it('should require an End date');
+  it('should require the start date to be older than the end date');
+});
