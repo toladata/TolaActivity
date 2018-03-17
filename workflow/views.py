e12c8436b90a8121e0261b54529a28bbd804ae8e
Mahmood Khan <mkhan@mercycorps.org>
2018-03-17 09:36:30 -0700

    updated sites list to work with bootstrap4

--- workflow/views.py
+++ workflow/views.py
@@ -1083,8 +1083,7 @@ class SiteProfileList(ListView):
     def dispatch(self, request, *args, **kwargs):
         if request.GET.has_key('report'):
             template_name = 'workflow/site_profile_report.html'
-        else:
-            template_name = 'workflow/site_profile_list.html'
+
         return super(SiteProfileList, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
@@ -1092,27 +1091,43 @@ class SiteProfileList(ListView):
         program_id = int(self.kwargs['program_id'])
 
         countries = getCountry(request.user)
-        getPrograms = Program.objects.all().filter(funding_status="Funded", country__in=countries)
+        getPrograms = Program.objects.filter(funding_status="Funded", country__in=countries)
 
         #this date, 3 months ago, a site is considered inactive
         inactiveSite = pytz.UTC.localize(datetime.now()) - relativedelta(months=3)
 
         #Filter SiteProfile list and map by activity or program
         if activity_id != 0:
-            getSiteProfile = SiteProfile.objects.all().prefetch_related('country','district','province').filter(projectagreement__id=activity_id).distinct()
+            getSiteProfile = SiteProfile.objects.prefetch_related(\
+                    'country','district','province')\
+                .filter(projectagreement__id=activity_id)\
+                .distinct()
         elif program_id != 0:
-            getSiteProfile = SiteProfile.objects.all().prefetch_related('country','district','province').filter(Q(projectagreement__program__id=program_id) | Q(collecteddata__program__id=program_id)).distinct()
+            getSiteProfile = SiteProfile.objects.prefetch_related(\
+                    'country','district','province')\
+                .filter(Q(projectagreement__program__id=program_id)\
+                        | Q(collecteddata__program__id=program_id))\
+                .distinct()
         else:
-            getSiteProfile = SiteProfile.objects.all().prefetch_related('country','district','province').filter(country__in=countries).distinct()
+            getSiteProfile = SiteProfile.objects.prefetch_related(\
+                    'country','district','province')\
+                .filter(country__in=countries)\
+                .distinct()
         if request.method == "GET" and "search" in request.GET:
-            """
-             fields = ('name', 'office')
-            """
-            getSiteProfile = SiteProfile.objects.all().filter(Q(country__in=countries), Q(name__contains=request.GET["search"]) | Q(office__name__contains=request.GET["search"]) | Q(type__profile__contains=request.GET['search']) |
-                                                            Q(province__name__contains=request.GET["search"]) | Q(district__name__contains=request.GET["search"]) | Q(village__contains=request.GET['search']) |
-                                                             Q(projectagreement__project_name__contains=request.GET["search"]) | Q(projectcomplete__project_name__contains=request.GET['search'])).select_related().distinct()
-        #paginate site profile list
+            getSiteProfile = SiteProfile.objects.filter(\
+                    Q(country__in=countries),\
+                    Q(name__contains=request.GET["search"])\
+                    | Q(office__name__contains=request.GET["search"])\
+                    | Q(type__profile__contains=request.GET['search'])\
+                    | Q(province__name__contains=request.GET["search"])\
+                    | Q(district__name__contains=request.GET["search"])\
+                    | Q(village__contains=request.GET['search'])
+                    | Q(projectagreement__project_name__contains=request.GET["search"])\
+                    | Q(projectcomplete__project_name__contains=request.GET['search']))\
+                .select_related()\
+                .distinct()
 
+        #paginate site profile list
         default_list = 10 # default number of site profiles per page
         user_list = request.GET.get('user_list') # user defined number of site profiles per page, 10, 20, 30
 
@@ -1120,17 +1135,23 @@ class SiteProfileList(ListView):
             default_list = int(user_list)
 
         paginator = Paginator(getSiteProfile, default_list)
-
         page = request.GET.get('page')
-
         try:
             getSiteProfile = paginator.page(page)
         except PageNotAnInteger:
             getSiteProfile = paginator.page(1)
         except EmptyPage:
             getSiteProfile = paginator.page(paginator.num_pages)
-
-        return render(request, self.template_name, {'inactiveSite':inactiveSite,'default_list':default_list,'getSiteProfile':getSiteProfile,'project_agreement_id': activity_id,'country': countries,'getPrograms':getPrograms, 'form': FilterForm(), 'helper': FilterForm.helper})
+        return render(request, self.template_name, {
+                'inactiveSite': inactiveSite,
+                'default_list': default_list,
+                'getSiteProfile':getSiteProfile,
+                'project_agreement_id': activity_id,
+                'country': countries,
+                'getPrograms':getPrograms,
+                'form': FilterForm(),
+                'helper': FilterForm.helper,
+                'user_list': user_list})
 
 
 class SiteProfileReport(ListView):
