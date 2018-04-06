92ff3137d5582ba6baeba9863734f10cbf56fb05
Kurt Wall <kwall@kurtwerks.com>
2018-04-06 11:14:50 -0700

    Add suites to all conf files
    Defined suites in all the configuration files. Updated the
    README to describe how to use them.

--- test/js/wdio.gecko.conf.js
+++ test/js/wdio.gecko.conf.js
@@ -6,6 +6,48 @@ exports.config = {
     exclude: [
         //'./test/specs/indicators_*.js'
     ],
+    suites: {
+        login: [
+            'test/specs/login.js',
+            'test/specs/password_invalid.js',
+            'test/specs/username_invalid.js'
+        ],
+        dashboard: [
+            'test/specs/dashboard.js'
+        ],
+        indicators: [
+            'test/specs/annual.js',
+            'test/specs/event.js',
+            'test/specs/lop_only.js',
+            'test/specs/mid-end_line.js',
+            'test/specs/monthly.js',
+            'test/specs/quarterly.js',
+            'test/specs/semiannual.js',
+            'test/specs/triannual.js',
+            'test/specs/count_indicators.js',
+            'test/specs/create_indicator_form.js',
+            'test/specs/filtering_indicators.js',
+            'test/specs/indicator_detail_form.js',
+            'test/specs/indicator_perf_metrics.js',
+            'test/specs/indicators_landing_page.js',
+            'test/specs/indicators_table.js',
+            'test/specs/pct_indicators.js',
+            'test/specs/sorting_indicators.js',
+            'test/specs/target_actuals.js'
+        ],
+        evidence: [
+            'test/specs/attach_evidence.js',
+            'test/specs/collected_data_form.js',
+            'test/specs/indicator_evidence_dropdown.js',
+            'test/specs/indicator_evidence_table.js'
+        ],
+        reports: [
+            'test/specs/export_reports.js',
+            'test/specs/grid_report.js',
+            'test/specs/prog_impact_assessment.js',
+            'test/specs/prog_impact_overview.js'
+        ]
+    },
     // Capabilities
     maxInstances: 1,
     capabilities: [
