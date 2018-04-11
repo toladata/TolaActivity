2cc8e50ab52c0f1f89123e314de9a92c6f57b31e
Daniel Avila <sparkytwobillion@gmail.com>
2018-04-11 15:11:15 -0700

    fixed test names

--- indicators/tests/test_generate_periodic_target.py
+++ indicators/tests/test_generate_periodic_target.py
@@ -13,18 +13,17 @@ class GeneratePeriodicTargetTests(TestCase):
         self.nth_target_period = 10
         self.target_frequency_custom = 5
 
-
-
-
     def test_lop_generate_periodic_target_single(self):
+        """Do we get back the expected result when we have an LOP?"""
         tf = Indicator.LOP
-        expected = {'period': 1}
+        expected = {'period': "Life of Program (LoP) only"}
         result = generate_periodic_target_single(tf, self.start_date,
                                                  self.nth_target_period,
                                                  target_frequency_custom=self.target_frequency_custom)
         self.assertDictEqual(expected, result)
 
-    def test_lop_generate_periodic_target_single(self):
+    def test_mid_generate_periodic_target_single(self):
+        """Do we get back the expected result when we have an MID_END?"""
         tf = Indicator.MID_END
         expected = [{'period': 'Midline'}, {'period': 'Endline'}]
         result = generate_periodic_target_single(tf, self.start_date,
@@ -32,7 +31,8 @@ class GeneratePeriodicTargetTests(TestCase):
                                                  target_frequency_custom=self.target_frequency_custom)
         self.assertEqual(expected, result)
 
-    def test_lop_generate_periodic_target_single(self):
+    def test_event_generate_periodic_target_single(self):
+        """Do we get back the expected result when we have an EVENT?"""
         tf = Indicator.EVENT
         expected = {'period': self.target_frequency_custom}
         result = generate_periodic_target_single(tf, self.start_date,
