d45e9261bc374e5180e1b3e27d75087643c6de7e
Daniel Avila <sparkytwobillion@gmail.com>
2018-04-11 15:39:17 -0700

    We now have tests ... need to go over the business rules for this function

--- indicators/tests/test_generate_periodic_target.py
+++ indicators/tests/test_generate_periodic_target.py
@@ -1,5 +1,6 @@
 import datetime
 
+from dateutil.relativedelta import relativedelta
 from django.test import TestCase
 
 from indicators.models import Indicator
@@ -9,7 +10,7 @@ from indicators.views import generate_periodic_target_single
 class GeneratePeriodicTargetTests(TestCase):
 
     def setUp(self):
-        self.start_date = datetime.datetime.now()
+        self.start_date = datetime.datetime(2018, 10, 5, 18, 00)
         self.nth_target_period = 10
         self.target_frequency_custom = 5
 
@@ -39,3 +40,67 @@ class GeneratePeriodicTargetTests(TestCase):
                                                  self.nth_target_period,
                                                  target_frequency_custom=self.target_frequency_custom)
         self.assertEqual(expected, result)
+
+    def test_annual(self):
+        """Do we get back the correct value if it is ANNUAL"""
+
+        tf = Indicator.ANNUAL
+        # TODO: Get clarification on the business rules for this function
+        expected = {'period': 'Year 11', 'end_date': '2029-10-04',
+                    'start_date': '2028-10-01'}
+
+        result = generate_periodic_target_single(tf, self.start_date,
+                                                 self.nth_target_period,
+                                        target_frequency_custom='')
+        self.assertDictEqual(expected, result)
+
+    def test_semi_annual(self):
+        tf = Indicator.SEMI_ANNUAL
+
+        # TODO: Get clarification on the business rules for this function
+        expected = {'end_date': '2024-04-04',
+                    'period': 'Semi-annual period 11',
+                    'start_date': '2023-10-01'}
+
+        result = generate_periodic_target_single(tf, self.start_date,
+                                                 self.nth_target_period,
+                                                 target_frequency_custom='')
+        self.assertDictEqual(expected, result)
+
+    def test_tri_annual(self):
+        tf = Indicator.TRI_ANNUAL
+        # TODO: Get clarification on the business rules for this function
+        expected = {'end_date': '2022-06-04',
+                    'period': 'Tri-annual period 11',
+                    'start_date': '2022-02-01'}
+
+        result = generate_periodic_target_single(tf, self.start_date,
+                                                 self.nth_target_period,
+                                                 target_frequency_custom='')
+        self.assertDictEqual(expected, result)
+
+    def test_quarterly(self):
+        tf = Indicator.QUARTERLY
+
+        # TODO: Get clarification on the business rules for this function
+        expected = {'end_date': '2021-07-04', 
+                    'period': 'Quarter 11', 
+                    'start_date': '2021-04-01'}
+
+        result = generate_periodic_target_single(tf, self.start_date,
+                                                 self.nth_target_period,
+                                                 target_frequency_custom='')
+        self.assertDictEqual(expected, result)
+
+    def test_monthly(self):
+        tf = Indicator.MONTHLY
+
+        # TODO: Get clarification on the business rules for this function
+        expected = {'end_date': '2019-09-04',
+                    'period': 'August 2019',
+                    'start_date': '2019-08-01'}
+
+        result = generate_periodic_target_single(tf, self.start_date,
+                                                 self.nth_target_period,
+                                        target_frequency_custom='')
+        self.assertDictEqual(expected, result)
