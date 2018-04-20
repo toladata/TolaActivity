6645439fe3964e8df5b61ceb77fa011e126cd44b
Kurt Wall <kwall@kurtwerks.com>
2018-04-19 21:04:54 -0700

    Normalize config files

--- test/js/wdio.gecko.conf.js
+++ test/js/wdio.gecko.conf.js
@@ -1,5 +1,5 @@
 exports.config = {
-    //execArgv: ['--inspect'],
+    execArgv: ['--inspect'],
     specs: [
         './test/specs/**/*.js'
     ],
@@ -81,9 +81,5 @@ exports.config = {
         ui: 'bdd',
         compilers: ['js:babel-register'],
         require: 'babel-register'
-    },
-    onComplete: function(exitCode, config, capabilities) {
-        var proc = require('child_process');
-        proc.execSync('/bin/rm -rfv /tmp/tats');
     }
 }
