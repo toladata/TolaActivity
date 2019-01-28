5c250c0ca46d45046837e79f17f00baf3c9b2b22
yilmazbekdemir <yilmaz@humanitec.com>
2019-01-28 17:42:49 +0100

    make evidence url string for collecteddata

--- indicators/models.py
+++ indicators/models.py
@@ -498,7 +498,7 @@ class CollectedData(models.Model):
     workflowlevel1 = models.ForeignKey(WorkflowLevel1, blank=True, null=True, related_name="i_workflowlevel1", help_text="Related workflowlevel1 if no workflowlevel 2 for this data and no indicator")
     date_collected = models.DateTimeField(null=True, blank=True, help_text="")
     comment = models.TextField("Comment/Explanation", max_length=255, blank=True, null=True)
-    evidence = models.ForeignKey(Documentation, null=True, blank=True, verbose_name="Evidence Document or Link", help_text="Evidence Document or Link")
+    evidence = models.CharField(max_length=255, null=True, blank=True, verbose_name="Evidence Document or Link", help_text="Evidence Document or Link")
     approved_by = models.ForeignKey(TolaUser, blank=True, null=True, verbose_name="Approved By", related_name="approving_data", help_text="Who approved collection")
     tola_table = models.ForeignKey(TolaTable, blank=True, null=True, help_text="If Track table was used link to it here")
     update_count_tola_table = models.BooleanField("Would you like to update the achieved total with the row count from TolaTables?",default=False, help_text="Update the unique count from Table if linked when updated")
